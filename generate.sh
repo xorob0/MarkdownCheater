1d90ea0e53390ed9066a57f1179bf000c1ab124d
Tim Simon <timothee@hackages.io>
2020-05-07 11:00:59 +0200

    Generating images (and much more)

--- generate.sh
+++ generate.sh
@@ -5,8 +5,39 @@
 #
 # Distributed under terms of the GPL license.
 #
+file=''
+output=''
+convertOutput=''
 
-file=$1
-filename="${file%.*}"
+parse_args() {
+		case "$1" in
+				-i)
+					file="$2"
+						;;
+				-o)
+						output="$2"
+						;;
+				-c)
+						convertOutput="$2"
+						;;
+				*)
+						echo "Unknown or badly placed parameter '$1'." 1>&2
+						exit 1
+						;;
+		esac
+}
 
-pandoc -t html "$file" --css style.css -o "$filename.pdf" --metadata pagetitle="$filename"
+while [ "$#" -ge 2 ]; do
+		parse_args "$1" "$2"
+		shift; shift
+done
+
+if [ -z "$output" ]; then
+	output="${file%.*}.pdf"
+fi
+
+pandoc -t html "$file" --css style.css -o "$output" --metadata pagetitle="$filename"
+
+if [ -n "$convertOutput" ]; then
+	convert "$output" "$convertOutput"
+fi
